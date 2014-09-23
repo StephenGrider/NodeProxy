427343eb0acb8c1d912ec24ca408d1cf808fb379
StephenGrider <ste.grider@gmail.com>
2014-09-22 22:37:32 -0700

    static route to google.com

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
-/* GET home page. */
-router.get('*', function(req, res) {
-  res.send('hi')
+
+router.get('*', function(req, response) {
+  options = {
+    host: 'http://www.google.com',
+    port: 80,
+    method: 'GET',
+  };
+
+  https.request(options, function(res){
+    var output = '';
+
+    res.on('error', function(err){
+      console.log(err);
+    });
+
+    res.on('data', function(chunk){
+      output += chunk;
+    });
+
+    res.on('end', function(res){
+      response.send(JSON.parse(res));
+    });
+
+  });
 });
 
 module.exports = router;
